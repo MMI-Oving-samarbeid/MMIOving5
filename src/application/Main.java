317f04980d6be416bed8dfce91391a9e749d2509
theakt <theakt@stud.ntnu.no>
2015-02-27 13:48:53 +0100

    Laget borderpane
    Forste commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,22 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setController(this);
+			Parent root = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("Kalender.fxml"));
+			
+			
+			Scene scene = new Scene(root,1600,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
